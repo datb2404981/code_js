e0b7315302e193b16b5b3218a9c41b8bf638d44f
3 det <datb2404981@student.ctu.edu.vn>
2025-03-26 22:54:03 +0700

    tạo file hello world

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 'use script'
-function getRandNumbers(min,max,length)
-{
-    let arr = new Array(length);
-    for (let i = 0; i < length; i++)
-    {
-        arr[i] = Math.random() * (max - min) + min;
-    }
-    return arr;
-}
 console.log("hello world");
